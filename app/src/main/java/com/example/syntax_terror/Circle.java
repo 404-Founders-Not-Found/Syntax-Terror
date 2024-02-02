9a3a64f42f01524989535f8b42d5e53fa4a0fdff
Min K <mkkim1@ualberta.ca>
2024-02-01 18:31:11 -0700

    The color IAN, I GOT IT

--- app/src/main/java/com/example/syntax_terror/Circle.java
+++ app/src/main/java/com/example/syntax_terror/Circle.java
@@ -1,26 +1,23 @@
 package com.example.syntax_terror;
-public abstract class Shape {
 
-    private String color = "Red";
+public class Circle {
 
     private int x;
     private int y;
 
+    public int getX() {
+        return x;
+    }
 
     public void setX(int x) {
         this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
     }
 
     public int getY() {
         return y;
     }
 
+    public void setY(int y) {
+        this.y = y;
+    }
 }
