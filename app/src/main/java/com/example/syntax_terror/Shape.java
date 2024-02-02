847e4799c4d8a8989731b26ac4347f6a835dfb92
Logan Omelchuk <72324766+hamsandvich@users.noreply.github.com>
2024-02-01 18:40:55 -0700

    Revert "added color string field"
    This reverts commit d5d5236fe48a7219fc0f0cf566aad4583e628fdf.

--- app/src/main/java/com/example/syntax_terror/Shape.java
+++ app/src/main/java/com/example/syntax_terror/Shape.java
@@ -3,7 +3,6 @@ package com.example.syntax_terror;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
 
     private String color = "Rainbow";
 
