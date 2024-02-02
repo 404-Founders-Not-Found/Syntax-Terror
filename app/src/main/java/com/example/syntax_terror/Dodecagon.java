7f616a259eca957a70601426b20608336d4db834
Reece King <reece@ualberta.ca>
2024-02-01 18:18:39 -0700

    Created a Dodecagon

---
+++ app/src/main/java/com/example/syntax_terror/Dodecagon.java
@@ -0,0 +1,17 @@
+package com.example.syntax_terror;
+
+public class Dodecagon extends Shape {
+    int radius;
+
+    public Dodecagon(int radius) {
+        this.radius = radius;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
