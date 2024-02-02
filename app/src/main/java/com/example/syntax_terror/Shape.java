ee6460238b57fe7c01cc6d590e6e986ac4ca0028
Ian <ihmorris@ualberta.ca>
2024-02-01 18:08:29 -0700

    TRANGLE

--- app/src/main/java/com/example/syntax_terror/Shape.java
+++ app/src/main/java/com/example/syntax_terror/Shape.java
@@ -3,7 +3,6 @@ public abstract class Shape {
     private int x;
     private int y;
 
-    private String color = "Maroon";
 
     public void setX(int x) {
         this.x = x;
@@ -21,11 +20,4 @@ public abstract class Shape {
         return y;
     }
 
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 }
